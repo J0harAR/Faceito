cf8000723cfe1be41af7496d98abe57796959de2
J0harAR <alfaro.ruiz.johan@gmail.com>
2022-10-26 00:43:06 -0500

    register

--- social/views.py
+++ social/views.py
@@ -1,10 +1,24 @@
+from email import message
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.forms import UserCreationForm,
+from django.contrib import message
 
 # Create your views here.
 def  feed(request):
     posts=Post.objects.all()
     return render(request,'social/feed.html',{"posts":posts})
+
+def register (request):
+        if request.method == 'POST':
+            form =UserCreationForm(request.POST)
+            if form.is_valid():
+                username=form.cleaned_data['username']
+
+                message.success(request,f'Usuario {username} creado')
+        else:
+             form =UserCreationForm()
+        return render(request,'social/register.html',{"form":form})
 
 def  profile(request):
     return render(request,'social/profile.html')
